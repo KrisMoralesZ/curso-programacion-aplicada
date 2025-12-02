bd6432ac050b50d7d39f38b44a7b14aa49ef9402
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-12-02 14:59:31 -0700

    Final Connection

--- Reporte/Data/AppDbContext.cs
+++ Reporte/Data/AppDbContext.cs
@@ -1,14 +1,19 @@
+using System;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Reporte.Models;
 
-namespace Reporte.Data;
-
-public class AppDbContext : DbContext
+namespace Reporte.Data
 {
-  public DbSet<Person> Persons { get; set; }
-
-  protected override void OnConfiguring(DbContextOptionsBuilder options)
+  public class AppDbContext : DbContext
   {
-    options.UseSqlite("Data Source=Reporte.db");
+    public DbSet<Person> Persons { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+      var dbPath = Path.Combine(AppContext.BaseDirectory, "Reporte.db");
+      options.UseSqlite($"Data Source={dbPath}");
+    }
+
   }
 }
--- Reporte/Models/Person.cs
+++ Reporte/Models/Person.cs
@@ -1,10 +1,10 @@
-namespace Reporte.Models;
-
-public class Person
+namespace Reporte.Models
 {
-  public int Id { get; set; }
-  public string Nombre { get; set; }
-  public string Apellido { get; set; }
-
-  public override string ToString() => $"{Nombre} {Apellido}";
+  public class Person
+  {
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public override string ToString() => $"{Nombre} {Apellido}";
+  }
 }
