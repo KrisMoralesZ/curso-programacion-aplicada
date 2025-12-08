b52c84249a470f6aad815c30590eb32ef5a553a5
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-12-07 18:37:26 -0700

    Update Login

--- Proyecto_Final/Windows/LoginWindow.axml.cs
+++ Proyecto_Final/Windows/LoginWindow.axml.cs
@@ -1,17 +1,42 @@
 using Avalonia.Controls;
-using Avalonia.Interactivity;
+using MsBox.Avalonia;
 
-namespace Proyecto_Final.Windows;
-
-public partial class LoginWindow : Window
+namespace Proyecto_Final.Windows
 {
+  public partial class LoginWindow : Window
+  {
     public LoginWindow()
     {
-        InitializeComponent();
+      InitializeComponent();
+      AuthService.CreateInitialAdmin();
     }
 
-    private void Login_Click(object? sender, RoutedEventArgs e)
+    private async void Login_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
-        
+      var username = UserBox.Text;
+      var password = PasswordBox.Text;
+
+      if (AuthService.Validate(username, password))
+      {
+        await MessageBoxManager
+            .GetMessageBoxStandard(
+                title: "Login correcto",
+                text: $"Bienvenido {username}")
+            .ShowAsync();
+
+        var mainWindow = new MainWindow();
+        mainWindow.Show();
+        this.Close();
+      }
+      else
+      {
+        await MessageBoxManager
+            .GetMessageBoxStandard(
+                title: "Error",
+                text: "Usuario o contraseña incorrectos"
+                )
+            .ShowAsync();
+      }
     }
+  }
 }
